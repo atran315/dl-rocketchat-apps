4f3ddf24f94ec2b976bec98bbbbcf2aa9e58a910
Andy Tran <andy.tran@neidinc.com>
2020-02-11 16:37:27 -0500

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # dl-rocketchat-apps
+
 A script to download all Rocket.Chat Marketplace apps
+
+This script will download all apps available in Rock.Chat Marketplace for you.
+All apps will be curl into .zip files so you can upload to your local Rocket.Chat server.
+
+Be sure to Enable Developer mode
---
+++ rc.apps.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Delete all old apps and rc.apps.json
+rm -rfv *.rc-app.zip
+rm -rfv rc.apps.json
+rm -rfv apps_list
+#
+echo "#################################################################################"
+echo "Downloads Rocket.Chat Marketplace apps list"
+echo "#################################################################################"
+curl https://marketplace.rocket.chat/v1/apps > rc.apps.json
+echo ""
+ls -hl rc.apps.json
+echo ""
+echo "Download is complete."
+echo ""
+# Get all app names
+cat rc.apps.json | jq -r '.[].latest.nameSlug' > apps_list
+APP_LIST='apps_list'
+echo ""
+echo "#################################################################################"
+echo "Start downloading apps."
+echo "#################################################################################"
+# Download apps
+while read app_name;
+do
+  # find app_id
+  app_id="$(cat rc.apps.json| jq '.[] | select(any(.latest; .nameSlug == "'$app_name'")) | .appId' | cut -d \" -f2)"
+  echo "App ID for $app_name is $app_id"
+  echo "Downloading $app_name app."
+  # curl apps into zip files
+  curl -L https://marketplace.rocket.chat/v1/apps/$app_id/download > $app_name.rc-app.zip
+  echo ""
+  echo "#################################################################################"
+done < $APP_LIST
+echo "#################################################################################"
+# List the downloaded apps via zip files.
+ls -lh *.zip
+echo "#################################################################################"
+echo "Enable developer mode in Rocket.Chat setting."
+echo "Upload the app_name .zip file you want to use."
+echo "#################################################################################"
